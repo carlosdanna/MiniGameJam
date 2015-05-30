f687180323ac7daa7d3faa7ee6fb7884ce60df5a
carlosdanna <carlos.danna@gmail.com>
2015-05-29 23:32:19 -0400

    Main menu art assets implemented and the Main menu fully functional missing name of the mini games and credits button. Talk to Carlos if there is any problem

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGame(string gameName)
+    {
+        Application.LoadLevel(gameName);
+    }
+}
