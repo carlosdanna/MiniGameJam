bea1710ef5f424a2cdf14b885b7f6f1cc9b9f48e
carlosdanna <carlos.danna@gmail.com>
2015-05-30 05:32:15 -0400

    UI stuff modifications for the game

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
+    public GameObject MainMenu;
+    public GameObject SelectGame;
+    public Text gameName;
 	// Use this for initialization
 	void Start () {
-	
+
+        if (PlayerPrefs.GetInt("ScreenToLoad") != 1 || PlayerPrefs.GetInt("ScreenToLoad") != 2)
+        {
+            PlayerPrefs.SetInt("ScreenToLoad", 1);  //if it is 1 = Main Menu 2 = Select Game
+        }
+        if (PlayerPrefs.GetInt("ScreenToLoad") == 1)
+        {
+            MainMenu.SetActive(true);
+            SelectGame.SetActive(false);
+        }
+        if (PlayerPrefs.GetInt("ScreenToLoad") == 2)
+        {
+            MainMenu.SetActive(false);
+            SelectGame.SetActive(true); 
+        }
 	}
 	
 	// Update is called once per frame
@@ -21,5 +39,16 @@ public class MainMenuController : MonoBehaviour {
     public void LoadGame(string gameName)
     {
         Application.LoadLevel(gameName);
+    }
+
+    public void Play()
+    {
+        LoadGame(gameName.text.ToString());
+    }
+
+    public void SelectedGame(Button b)
+    {
+        gameName.text = b.gameObject.name;
+        b.GetComponent<Button>().Select();
     }
 }
