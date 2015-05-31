4a7eb16ff8d82513bdaa0c98895ecfdb3c6bee03
Spencer Slaton <burned05@hotmail.com>
2015-05-31 14:12:30 -0400

    Player Created

---
+++ Assets/Resources/ProjectJungle/Scripts/_JGPlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _JGPlayerScript : MonoBehaviour {
+	public string key;
+	public float speed = 0.1f;
+	public float dropSpeed = 0.02f;
+	float velocity;
+	// Use this for initialization
+	void Start () {
+		velocity = -dropSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity = -dropSpeed;
+		switch (key) {
+		case "Q":
+			if(Input.GetKeyDown(KeyCode.Q))
+				velocity = speed;
+			break;
+		case "T":
+			if(Input.GetKeyDown(KeyCode.T))
+				velocity = speed;
+			break;
+		case "I":
+			if(Input.GetKeyDown(KeyCode.I))
+				velocity = speed;
+			break;
+		case "7":
+			if(Input.GetKeyDown(KeyCode.Keypad7))
+				velocity = speed;
+			break;
+		}
+
+		transform.position = new Vector3 (transform.position.x, transform.position.y + velocity, transform.position.z);
+	}
+}
